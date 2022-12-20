631cb45349244d63dcd015c416d9d231adda3dbb
AhmedZatar97 <Ahmad.Zatar@optimumpartners.co>
2022-12-20 10:29:20 +0300

    add update validation

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,36 +1,36 @@
-const Router = require("express-promise-router");
+const Router = require('express-promise-router');
 
-const { register, login, getUserData, deleteUser, updateUser } = require("../controllers/user");
-const validation = require("../middleware/validation");
-const auth = require("../middleware/auth");
-const authValidator = require("../validation/user");
+const { register, login, getUserData, deleteUser, updateUser } = require('../controllers/user');
+const validation = require('../middleware/validation');
+const auth = require('../middleware/auth');
+const authValidator = require('../validation/user');
 
 const router = Router({ mergeParams: true });
-const { registerSchema, loginSchema } = authValidator;
+const { registerSchema, loginSchema, updateSchema } = authValidator;
 
 /**
  * Handle POST to /api/v1/users/register route.
  */
-router.post("/register", validation(registerSchema, "body"), register);
+router.post('/register', validation(registerSchema, 'body'), register);
 
 /**
  * Handle POST to /api/v1/users/login route.
  */
-router.post("/login", validation(loginSchema, "body"), login);
+router.post('/login', validation(loginSchema, 'body'), login);
 
 /**
  * Handle GET to /api/v1/users/ route.
  */
-router.get("/", auth, getUserData);
+router.get('/', auth, getUserData);
 
 /**
  * Handle Delete to /api/v1/users/ route.
  */
- router.delete("/", auth, deleteUser);
+router.delete('/', auth, deleteUser);
 
 /**
  * Handle Put to /api/v1/users/ route.
  */
- router.put("/", auth, updateUser);
+router.put('/', auth, validation(updateSchema, 'body'), updateUser);
 
 module.exports = router;
